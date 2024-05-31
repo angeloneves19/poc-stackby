a76e0de370d0c3ca27fcd05e45eb088f68165f36
angeloneves19 <angelorogeriodasilvaneves2005@gmail.com>
2024-05-31 13:56:40 -0300

    primeiro commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,35 @@
 'use client';
 import Image from "next/image"
 import react from "../../public/assets/react.png"
-import js from "../../public/assets/js.png"
-import next from "../../public/assets/next.png"
 import { AppBar, Box, Button, Card, CardActionArea, CardContent, CssBaseline, IconButton, Toolbar, Typography } from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
 import { ThemeConfig } from '@/componentes/config/theme';
+import { useEffect, useState } from "react";
 
 const drawerWidth = 0;
 
 export default function Home() {
+  const [data, setData] = useState <any[]>([])
+  useEffect (()=>{
+    async function listagemTemas() {
+      try {
+        const data = await fetch("https://stackby.com/api/betav1/rowlist/stqB2IjOCulBJkhrZB/Nivelamento", {
+          method:"GET", 
+          headers: {
+            "x-api-key": "jOELi8SJTNSw0qVy",
+            "Content-Type": "application/json"
+          }
+        })
+        const imprimeData = await data.json()
+        setData(imprimeData)
+        console.log(imprimeData)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    listagemTemas()
+  } ,[])
+
   return (
     <main>
       <ThemeConfig>
@@ -32,7 +52,6 @@ export default function Home() {
               >
                 <MenuIcon />
               </IconButton>
-
               <Typography variant="h6" noWrap component="div" flexGrow={1} sx={{ cursor: "pointer" }}>
                 Home
               </Typography>
@@ -60,69 +79,32 @@ export default function Home() {
             sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
           >
             <Toolbar />
-            <CardContent>
-              <Typography variant="h5" component="div" color="black">
-                React
-              </Typography>
-              <Typography variant="body2" color="black">
-                11/02/2024
-              </Typography>
+            <div>
+              {data.map((element, index) => ( 
+                <div key={index}>
+                  <CardContent>
+                    <Typography variant="h5" component="div" color="black">
+                     {element}
+                    </Typography>
+                    <Typography variant="body2" color="black">
+                      11/02/2024
+                    </Typography>
 
-              <Typography variant="body2" color="black">
-                Resume: Aula avançada de React.
-              </Typography>
-            </CardContent>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea>
-                <Image
-                  width={300}
-                  src={react}
-                  alt="imagem filme"
-                />
-              </CardActionArea>
-            </Card>
-            <CardContent>
-              <Typography variant="h5" component="div" color="black">
-                JavaScript
-              </Typography>
-              <Typography variant="body2" color="black">
-                01/02/2024
-              </Typography>
-
-              <Typography variant="body2" color="black">
-                Resume: Aula avançada de JavaScript.
-              </Typography>
-            </CardContent>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea>
-                <Image
-                  width={300}
-                  src={js}
-                  alt="imagem filme"
-                />
-              </CardActionArea>
-            </Card>
-            <CardContent>
-              <Typography variant="h5" component="div" color="black">
-                Next.js
-              </Typography>
-              <Typography variant="body2" color="black">
-                13/02/2024
-              </Typography>
-
-              <Typography variant="body2" color="black">
-                Resume: Aula avançada de next.js.
-              </Typography>
-            </CardContent>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea>
-                <Image
-                  width={300}
-                  src={next}
-                  alt="imagem filme"
-                />
-              </CardActionArea>
-            </Card>
+                    <Typography variant="body2" color="black">
+                      Resume: Aula avançada de React.
+                    </Typography>
+                  </CardContent><Card sx={{ maxWidth: 345 }}>
+                    <CardActionArea>
+                      <Image
+                        width={300}
+                        src={react}
+                        alt="imagem filme" />
+                    </CardActionArea>
+                  </Card>
+                </div>
+              ))}
+            </div>
+            
           </Box>
         </Box>
       </ThemeConfig>
